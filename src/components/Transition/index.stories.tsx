5fe57bfa918fdfdbcb22307c01b907b32fcf9a3c
Phat Thaveepholcharoen <overtone00445@gmail.com>
2022-12-07 23:58:18 +0700

    add Toggle case

---
+++ src/components/Transition/index.stories.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+import { motion, AnimatePresence } from "framer-motion";
+import styled from "@emotion/styled";
+
+export default {
+  title: "components/Transition",
+};
+
+export const ToggleElements = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(!isOpen)}>Toggle</button>
+
+      <div style={{ position: "relative" }}>
+        <AnimatePresence>
+          {isOpen ? (
+            <motion.div
+              key="open"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+              style={{
+                color: "white",
+                padding: 12,
+                background: "black",
+                position: "absolute",
+              }}
+            >
+              Open
+            </motion.div>
+          ) : (
+            <motion.div
+              key="close"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+              style={{
+                color: "white",
+                padding: 12,
+                background: "black",
+                position: "absolute",
+              }}
+            >
+              Close
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </>
+  );
+};
