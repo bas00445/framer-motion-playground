8a3c01ef1fbd37723313208be48555794951a6b3
bas.phat <phat.phat@bitkub.com>
2022-12-08 21:38:25 +0700

    update storybook config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,29 @@
+const { mergeConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
+const svgr = require("vite-plugin-svgr");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      resolve: {
+        alias: { foo: "bar" },
+      },
+      plugins: [react(), svgr()],
+    });
+  },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+};
